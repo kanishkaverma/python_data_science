7132709bc1be2bd6892ceedfe4178e1f1cbc2ec2
kanishka verma <kanishkaverma@users.noreply.github.com>
2021-01-04 08:35:50 +0530

    Performed some stat analysis on my weight change using pandas and matplotlib ✔✔✔.

---
+++ practice/prac.py
@@ -0,0 +1,33 @@
+#%%
+#%%
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(0,len(nums)):
+            for j in range(i, len(nums)):
+
+
+                if nums[i]+nums[j]==target:
+                    return i,j
+ 
+
+
+
+# %%
+sol = Solution()
+print(sol.twoSum([3,2,4],6))
+
+
+
+#%%
+if target_num == nums[i]: 
+                print(nums.index(target_num,i)
+
+                if target_num != nums[i]:
+                    answer.append(nums.index(target_num, i))
+                elif answer:
+                    answer.append(i)
+                    break
+                else: 
+                    print("lol")
+            else: 
+                pass
